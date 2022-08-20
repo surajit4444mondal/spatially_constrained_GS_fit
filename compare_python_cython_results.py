f30953a3603d252c00b2d82ab38fdf77a708f619
Surajit Mondal <surajit4444mondal@gmail.com>
2022-08-20 12:35:17 -0400

    Compared python-cython outputs.

--- compare_python_cython_results.py
+++ compare_python_cython_results.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 cython_file='fitted_param_maps.hdf5'
 python_file='fitted_param_maps_python.hdf5'
 
-key='Bx100G'
+key='chi_sq'
 
 cython_hf=h5py.File(cython_file)
 cython_param=np.array(cython_hf[key])[0,:,:]
@@ -20,14 +20,17 @@ fig=plt.figure()
 ax=fig.add_subplot(131)
 plt.imshow(cython_param,origin='lower')
 plt.colorbar()
+plt.title("Cython output")
 
 ax=fig.add_subplot(132)
 plt.imshow(python_param,origin='lower')
 plt.colorbar()
+plt.title("Python output")
 
 ax=fig.add_subplot(133)
 plt.imshow(python_param/cython_param,origin='lower')
 plt.colorbar()
+plt.title("Python/Cython")
 
-
+plt.savefig("chisq_comparison.png")
 plt.show()
