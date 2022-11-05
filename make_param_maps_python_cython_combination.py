bc80441c31831578a0f5ca1319d3bf08dc4f741d
Surajit Mondal <surajit4444mondal@gmail.com>
2022-11-04 20:00:23 -0400

    Change for regular usage

--- make_param_maps_python_cython_combination.py
+++ make_param_maps_python_cython_combination.py
@@ -42,7 +42,7 @@ min_freq_num=35
 smooth_lengths=[0.5,1.0]
 discontinuity_thresh=5.0
 max_dist_parameter_space=4
-outfile="final_param_map_image_new_image_smoothing.hdf5"
+outfile="final_param_map_image_smoothing_0.5_scale.hdf5"
 
 
 cell=0.5  ###arcsec
@@ -56,22 +56,18 @@ for i in range(num_freqs):
 spectrum_files=['/home/surajit/Downloads/20210507/eovsa_data/time_'+time_slice+'_scaled.fits']
 
 model=Model(model_file)
-
+'''
 fitted=np.load("test_fitted.npy")
 low_freq_ind=np.load("test_low_freq_ind.npy")
 upper_freq_ind=np.load("test_upper_freq_ind.npy")
-
+'''
 pf.main_func(xmin,ymin,xmax,ymax,lowest_freq,highest_freq,min_freq_num,spectrum_files,model,resolution, smooth_lengths,\
 		sys_error=sys_error,rms_thresh=rms_thresh,discontinuity_thresh=discontinuity_thresh,\
-		max_dist_parameter_space=max_dist_parameter_space, outfile=outfile,image_smoothing=True,pixel_fit=False,\
-		discont_removal=False,cluster_removal=False,fitted=fitted,low_freq_ind=low_freq_ind,upper_freq_ind=upper_freq_ind)
+		max_dist_parameter_space=max_dist_parameter_space, outfile=outfile,image_smoothing=True,\
+		discont_removal=True,cluster_removal=True)
 
 
 
-#hf=h5py.File("param_map_190200_190210_new.hdf5")
-#fitted=np.load("python_cython_comb_test.npy")
-#low_freq_ind=np.ravel(np.array(hf['low_freq_ind'],dtype=np.intc))
-#upper_freq_ind=np.ravel(np.array(hf['upper_freq_ind'],dtype=np.intc))
-#hf.close()
+
 
 
